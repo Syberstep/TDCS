22ad7e9c5ca20aa05a2ed436dd79c7262c107444
unknown <you@example.com>
2015-11-13 12:27:17 +0700

    checkScore success edit by job

--- src/main/java/com/springapp/mvc/controller/exam/ResultController.java
+++ src/main/java/com/springapp/mvc/controller/exam/ResultController.java
@@ -42,20 +42,20 @@ public class ResultController {
     @Autowired
     QueryPaperQuestionDomain queryPaperQuestionDomain;
 
-//    @Transactional
+    //    @Transactional
     @RequestMapping(method = RequestMethod.GET, value = "/exam/checkScore")
-    public String checkScore(HttpServletRequest request,ModelMap modelMap){
+    public String checkScore(HttpServletRequest request, ModelMap modelMap) {
 
         List<ExamResult> examResults = queryExamResultDomain.getUserResult(queryUserDomain.getCurrentUser(request));
 
-        modelMap.addAttribute("examResults",examResults);
+        modelMap.addAttribute("examResults", examResults);
 
         return "checkScore";
     }
 
-    @RequestMapping(method= RequestMethod.POST, value="/exam/checkScore/getResultDetail")
+    @RequestMapping(method = RequestMethod.POST, value = "/exam/checkScore/getResultDetail")
     @ResponseBody
-    public ResponseEntity<String> getResultDetail(@RequestParam(value = "resultId",required = true)Integer resultId){
+    public ResponseEntity<String> getResultDetail(@RequestParam(value = "resultId", required = true) Integer resultId) {
 
         ExamResult examResult = queryExamResultDomain.getExamResultById(resultId);
 //        if(examResult.getStatus().getId() != 7){
@@ -71,7 +71,7 @@ public class ResultController {
     }
 
 
-//
+    //
 //    @RequestMapping(method = RequestMethod.POST, value = "/exam/addQuestion")
 //    @ResponseBody
 //    public ResponseEntity<String> getUserResult(ModelMap model,HttpServletResponse response,HttpServletRequest request){
@@ -84,111 +84,169 @@ public class ResultController {
 //
 //        return new ResponseEntity<String>(json, headers, HttpStatus.OK);
 //    }
-@RequestMapping(method= RequestMethod.POST, value="/exam/checkScore/getResultDetail2")
-@ResponseBody
-public ResponseEntity<String> getResultDetail2(@RequestParam(value = "resultId",required = true)Integer resultId){
+    @RequestMapping(method = RequestMethod.POST, value = "/exam/checkScore/getResultDetail2")
+    @ResponseBody
+    public ResponseEntity<String> getResultDetail2(@RequestParam(value = "resultId", required = true) Integer resultId) {
 
-    ExamResult examResult = queryExamResultDomain.getExamResultById(resultId);
-    Set<SubCategory> subCatSet = new HashSet<SubCategory>();
-    Map<SubCategory,Float> resultObjectScoreSubcatMap = new HashMap<SubCategory, Float>();
-    Map<SubCategory,Float> maxScoreSubCatObjectiveMap = new HashMap<SubCategory, Float>();
-    Map<SubCategory,Float> maxScoreSubCatSubjectiveMap = new HashMap<SubCategory, Float>();
-    Map<SubCategory,Float> resultSubjectScoreSubcatMap = new HashMap<SubCategory, Float>();
-    List<CheckScore> checkScoreList = new ArrayList<CheckScore>();
+        ExamResult examResult = queryExamResultDomain.getExamResultById(resultId);
+        List<SubCategory> subCatList = new ArrayList<SubCategory>();
+        Map<SubCategory, Float> resultScoreSubcatMap = new HashMap<SubCategory, Float>();
+        Map<SubCategory, Float> maxScoreSubCatMap = new HashMap<SubCategory, Float>();
+        List<CheckScore> checkScoreList = new ArrayList<CheckScore>();
 
-    Set<PaperQuestion> pqList = examResult.getExamRecord().getPaper().getQuestions();
-    List<ExamAnswerRecord> examAnswerRecords = examResult.getExamRecord().getExamAnswerRecords();
+        Set<PaperQuestion> pqList = examResult.getExamRecord().getPaper().getQuestions();
+        List<ExamAnswerRecord> examAnswerRecords = examResult.getExamRecord().getExamAnswerRecords();
 
-    for(PaperQuestion pq : pqList){
-        subCatSet.add(pq.getQuestion().getSubCategory());
-    }
+        for (PaperQuestion pq : pqList) {
+            subCatList.add(pq.getQuestion().getSubCategory());
+        }
 
+        Set<SubCategory> subCatSet = new HashSet<SubCategory>(subCatList);
 
-//-------------------------------------------------------------------------------------------------------------------
-    Float maxScoreObjective = 0f;
-    Float maxScoreSubjective = 0f;
-    Float resultObjectScore = 0f;
-    Float resultSubjectScore = 0f;
-    for(SubCategory sc : subCatSet){
-        CheckScore checkScore = new CheckScore();
-        for(ExamAnswerRecord ear : examAnswerRecords){
-            PaperQuestion pq = queryPaperQuestionDomain.getPaperQuestion(examResult.getExamRecord().getPaper(), ear.getQuestion());
-            float objective = 0f;
-            float subjective = 0f;
-            float maxScoreObject = 0f;
-            float maxScoreSubject = 0f;
-            if(ear.getQuestion().getSubCategory() == sc){
-                if(ear.getAnswerObjective() != null){
-                    if(ear.getAnswerObjective().getCorrection()){
-                        objective = pq.getScore();
-                        resultObjectScore += objective;
-                        resultObjectScoreSubcatMap.put(sc, resultObjectScore);
-                    }
-                    maxScoreObject = pq.getScore();
-                    maxScoreObjective += maxScoreObject;
-                    maxScoreSubCatObjectiveMap.put(sc,maxScoreObjective);
-                }else{
-                    if(ear.getMarkingRecord() != null){
-                        if(ear.getMarkingRecord().getMarkingScore() != 0){
-                            subjective = ear.getMarkingRecord().getMarkingScore();
-                            resultSubjectScore = subjective;
-                            resultSubjectScoreSubcatMap.put(sc,resultSubjectScore);
+        int count = 0;
+
+        for (SubCategory sc : subCatSet) {
+            Float maxScore = 0f;
+            Float resultScore = 0f;
+
+            CheckScore checkScore = new CheckScore();
+            for (ExamAnswerRecord ear : examAnswerRecords) {
+                PaperQuestion pq = queryPaperQuestionDomain.getPaperQuestion(examResult.getExamRecord().getPaper(), ear.getQuestion());
+
+                if (ear.getQuestion().getSubCategory().equals(sc)) {
+                    if (ear.getAnswerObjective() != null) {
+                        if (ear.getAnswerObjective().getCorrection()) {
+                            resultScore += pq.getScore();
                         }
-                        maxScoreSubject = pq.getScore();
-                        maxScoreSubjective += maxScoreSubject;
+
+                    } else {
+                        if (ear.getMarkingRecord() != null) {
+                            if (ear.getMarkingRecord().getMarkingScore() != 0 || ear.getMarkingRecord().getMarkingScore() == 0) {
+                                resultScore = ear.getMarkingRecord().getMarkingScore();
+                            }
+                        }
                     }
-                    maxScoreSubCatSubjectiveMap.put(sc,maxScoreSubjective);
+                    maxScore += pq.getScore();
                 }
-//                resultSubjectScoreSubcatMap.put(sc,resultSubjectScore);
-//                resultObjectScoreSubcatMap.put(sc, resultObjectScore);
-//                maxScoreSubCatObjectiveMap.put(sc,maxScoreObjective);
-//                maxScoreSubCatSubjectiveMap.put(sc,maxScoreSubjective);
             }
 
-        }
+            resultScoreSubcatMap.put(sc, resultScore);
+            maxScoreSubCatMap.put(sc, maxScore);
 
+            checkScore.setCategoryName(sc.getCategory().getName());
+            checkScore.setSubCategoryName(sc.getName());
 
-        checkScore.setCategoryName(sc.getCategory().getName());
-        checkScore.setSubCategoryName(sc.getName());
-        for(SubCategory key : resultObjectScoreSubcatMap.keySet()){
-            if( key.getName() == sc.getName()){
-                Float value = resultObjectScoreSubcatMap.get(key);
-                checkScore.setSubCategoryResultObjectScore(value);
+            for (SubCategory key : resultScoreSubcatMap.keySet()) {
+                if (key.getName() == sc.getName()) {
+                    Float value = resultScoreSubcatMap.get(key);
+                    checkScore.setSubCategoryResultScore(value);
+                }
             }
+
+            for (SubCategory key : maxScoreSubCatMap.keySet()) {
+                if (key.getName() == sc.getName()) {
+                    Float value = maxScoreSubCatMap.get(key);
+                    checkScore.setSubCategoryMaxScore(value);
+                }
+            }
+            if(count == 0){
+                checkScore.setSubjectiveScore(examResult.getSubjectiveScore());
+                checkScore.setComment(examResult.getComment());
+                checkScore.setObjectiveScore(examResult.getObjectiveScore());
+                if(examResult.getMarkedBy()==null){
+                    checkScore.setMarkedBy("HRMS System");
+                }else{
+                    checkScore.setMarkedBy(examResult.getMarkedBy().getThFname());
+                }
+
+            }
+
+            count++;
+            checkScoreList.add(checkScore);
         }
-        for(SubCategory key : resultSubjectScoreSubcatMap.keySet()){
-            if( key.getName() == sc.getName()){
-                Float value = resultSubjectScoreSubcatMap.get(key);
-                checkScore.setSubCategoryResultSubjectScore(value);
-            }
-        }
-        for(SubCategory key : maxScoreSubCatObjectiveMap.keySet()){
-            if( key.getName() == sc.getName()){
-                Float value = maxScoreSubCatObjectiveMap.get(key);
-                checkScore.setSubCategoryMaxScoreObjective(value);
-            }
-        }for(SubCategory key : maxScoreSubCatSubjectiveMap.keySet()){
-            if( key.getName() == sc.getName()){
-                Float value = maxScoreSubCatSubjectiveMap.get(key);
-                checkScore.setSubCategoryMaxScoreSubjective(value);
-            }
-        }
-//        checkScore.setObjectiveScore(examResult.getObjectiveScore().doubleValue());
-//        checkScore.setSubjectiveScore(examResult.getSubjectiveScore().doubleValue());
-        checkScoreList.add(checkScore);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json;charset=UTF-8");
+        String json = new JSONSerializer().include("examRecord.examAnswerRecords.answerObjective.question").exclude("*.class")
+                .exclude("examRecord.paper").exclude("*.status").serialize(checkScoreList);
+
+        return new ResponseEntity<String>(json, headers, HttpStatus.OK);
     }
-//-------------------------------------------------------------------------------------------------------------------
-    HttpHeaders headers = new HttpHeaders();
-    headers.add("Content-Type", "application/json;charset=UTF-8");
-    String json = new JSONSerializer().include("examRecord.examAnswerRecords.answerObjective.question").exclude("*.class")
-            .exclude("examRecord.paper").exclude("*.status").serialize(checkScoreList);
-//String json = new JSONSerializer().include("examRecord.examAnswerRecords.question.subCategory").include("examRecord.examAnswerRecords").exclude("*.class")
-//            .exclude("examRecord.paper").exclude("*.status").serialize(examResult);
-
-    return new ResponseEntity<String>(json, headers, HttpStatus.OK);
-
 }
 
 
+class CheckScore {
+
+    private String categoryName;
+    private String subCategoryName;
+    private Float subCategoryResultScore;
+    private Float subCategoryMaxScore;
+    private String comment;
+    private Float objectiveScore;
+    private Float subjectiveScore;
+    private String markedBy;
+
+    public String getMarkedBy() {
+        return markedBy;
+    }
+
+    public void setMarkedBy(String createBy) {
+        this.markedBy = createBy;
+    }
+
+    public Float getSubCategoryResultScore() {
+        return subCategoryResultScore;
+    }
+
+    public void setSubCategoryResultScore(Float subCategoryResultScore) {
+        this.subCategoryResultScore = subCategoryResultScore;
+    }
+
+    public Float getSubCategoryMaxScore() {
+        return subCategoryMaxScore;
+    }
+
+    public void setSubCategoryMaxScore(Float subCategoryMaxScore) {
+        this.subCategoryMaxScore = subCategoryMaxScore;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Float getObjectiveScore() {
+        return objectiveScore;
+    }
+
+    public void setObjectiveScore(Float objectiveScore) {
+        this.objectiveScore = objectiveScore;
+    }
+
+    public Float getSubjectiveScore() {
+        return subjectiveScore;
+    }
+
+    public void setSubjectiveScore(Float subjectiveScore) {
+        this.subjectiveScore = subjectiveScore;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public String getSubCategoryName() {
+        return subCategoryName;
+    }
+
+    public void setSubCategoryName(String subCategoryName) {
+        this.subCategoryName = subCategoryName;
+    }
 
 }
--- src/main/webapp/WEB-INF/pages/exam/modal/showScoreModal.jsp
+++ src/main/webapp/WEB-INF/pages/exam/modal/showScoreModal.jsp
@@ -9,82 +9,127 @@
 <%--Modal Check Score--%>
 <!-- alertModal-->
 <%--<div id="alertModalResult" class="modal fade">--%>
-  <%--<div class="modal-dialog">--%>
-    <%--<div class="modal-content">--%>
-      <%--<div class="modal-header">--%>
-        <%--<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>--%>
-        <%--<h4 class="modal-title">Modal title</h4>--%>
-      <%--</div>--%>
-      <%--<div class="modal-body">--%>
-        <%--<p>One fine body&hellip;</p>--%>
-        <%----%>
-      <%--</div>--%>
-      <%--<div class="modal-footer">--%>
-        <%--<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>--%>
-        <%--<button type="button" class="btn btn-primary">Save changes</button>--%>
-      <%--</div>--%>
-    <%--</div><!-- /.modal-content -->--%>
-  <%--</div><!-- /.modal-dialog -->--%>
+<%--<div class="modal-dialog">--%>
+<%--<div class="modal-content">--%>
+<%--<div class="modal-header">--%>
+<%--<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>--%>
+<%--<h4 class="modal-title">Modal title</h4>--%>
+<%--</div>--%>
+<%--<div class="modal-body">--%>
+<%--<p>One fine body&hellip;</p>--%>
+<%----%>
+<%--</div>--%>
+<%--<div class="modal-footer">--%>
+<%--<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>--%>
+<%--<button type="button" class="btn btn-primary">Save changes</button>--%>
+<%--</div>--%>
+<%--</div><!-- /.modal-content -->--%>
+<%--</div><!-- /.modal-dialog -->--%>
 <%--</div><!-- /.modal -->--%>
-<div id="alertModalResult" class="modal fade bs-example-modal-lg"  role="dialog" aria-labelledby="myLargeModalLabel">
-  <div class="modal-dialog modal-lg">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button class="close" aria-label="Close" data-dismiss="modal" type="button">
-          <span aria-hidden="true">×</span>
-        </button>
-        <h4 id="myLargeModalLabel" class="modal-title">Large modal</h4>
-      </div>
-      <div class="modal-body">
-        <div class="container">
-          <div class="row">
-            <h5>รายละเอียด  </h5>
-            <table  class="table table-bordered table-responsive table-hover">
-              <thead class="bg-info small">
-              <tr>
-                <th class="text-center">หมวดหมู่</th>
-                <th class="text-center">หัวข้อเรื่อง</th>
-                <th class="text-center">คะเเนนเต็ม</th>
-                <th class="text-center">คะแนนที่ได้</th>
-              </tr>
-              </thead>
-
-              <!---------------------------------------------------- Generate Table --------------------------------------------------------------------------------->
-              <tbody id="tbodyCheckScore">
-              </tbody>
-              <%--<div id="paperNotFound" width="100%">--%>
-              <%--<h3 id="paperNotFoundDesc">ไม่พบชุดข้อสอบ</h3>--%>
-              <%--</div>--%>
-            </table>
-          </div>
-          <div class="row">
-            <div class="col-md-3 text-right">
-              <h5>ความคิดเห็น :</h5>
+<div id="alertModalResult" class="modal fade bs-example-modal-lg" role="dialog" aria-labelledby="myLargeModalLabel">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button class="close" aria-label="Close" data-dismiss="modal" type="button">
+                    <span aria-hidden="true">×</span>
+                </button>
+                <h4 id="myLargeModalLabel" class="modal-title">คะแนนสอบ</h4>
             </div>
-            <div class=" col-md-7">
+            <div class="modal-body">
+                <div class="container">
+                    <div class="row">
+                        <h5>รายละเอียด </h5>
+                        <table class="table table-bordered table-responsive table-hover">
+                            <thead class="bg-info small">
+                            <tr>
+                                <th class="text-center">หมวดหมู่</th>
+                                <th class="text-center">หัวข้อเรื่อง</th>
+                                <th class="text-center">คะเเนนเต็ม</th>
+                                <th class="text-center">คะแนนที่ได้</th>
+                            </tr>
+                            </thead>
+
+                            <!---------------------------------------------------- Generate Table --------------------------------------------------------------------------------->
+                            <tbody id="tbodyCheckScore">
+                            </tbody>
+                            <%--<div id="paperNotFound" width="100%">--%>
+                            <%--<h3 id="paperNotFoundDesc">ไม่พบชุดข้อสอบ</h3>--%>
+                            <%--</div>--%>
+                        </table>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-3 text-right">
+                            <h5 >ปรนัย :</h5>
+                        </div>
+                        <div class="col-md-1">
+                            <h5 id="forObjScore"></h5>
+                        </div>
+                        <div class="col-sm-offset-1 col-sm 1">
+                            <h5 class ="txtScore"></h5>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-3 text-right">
+                            <h5>อัตนัย :</h5>
+                        </div>
+                        <div class="col-md-1">
+                            <h5 id="forsjScore"></h5>
+                        </div>
+                        <div class="col-sm-offset-1 col-sm 1">
+                            <h5 class ="txtScore"></h5>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-3 text-right">
+                            <h4>รวม :</h4>
+                        </div>
+                        <div class="col-md-1">
+                            <h5 id="forSumScore"></h5>
+                        </div>
+                        <div class="col-sm-offset-1 col-sm 1">
+                            <h5 class ="txtScore"></h5>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-3 text-right">
+                            <h4>เต็ม :</h4>
+                        </div>
+                        <div class="col-md-1">
+                            <h5 id="forMaxScore"></h5>
+                        </div>
+                        <div class="col-sm-offset-1 col-sm 1">
+                            <h5 class ="txtScore"></h5>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="row">
+                        <div class="col-md-3 text-right">
+                            <h5>ความคิดเห็น :</h5>
+                        </div>
+                        <div class=" col-md-7">
               <textarea id="commentTextArea" class="form-control" rows="5" disabled
                         style="resize: none; background-color: #FFFFFF;"></textarea>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-2 col-md-offset-6 text-right">
+                            <h5>ตรวจโดย :</h5>
+                        </div>
+                        <div class="col-md-4">
+                            <h5 id="markedByShow"></h5>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-          <div class="row">
-            <div class="col-md-2 col-md-offset-6 text-right">
-              <h5>ตรวจโดย :</h5>
-            </div>
-            <div class="col-md-4">
-              <h5 id="markedByShow"></h5>
-            </div>
-          </div>
         </div>
-      </div>
     </div>
-  </div>
 </div>
 <script>
 
-  $('.showScoreCloseBtn').on('click',function(){
-    $("#showScore").modal("hide");
-  })
+    $('.showScoreCloseBtn').on('click', function () {
+        $("#showScore").modal("hide");
+    })
 
-  $('#objectiveShowScore').text
+    $('#objectiveShowScore').text
 
 </script>
